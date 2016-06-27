4139d238247e39888a17381726bfe2418321e305
Chris Heller <ctheller12@gmail.com>
2016-06-27 17:31:36 -0400

    db seeded. Working on swig template for index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var Promise = require('bluebird');
+var Hotel = models.Hotel; 
+var Restaurant = models.Restaurant;
+var Activity = models.Activity;
+
+
 
 router.get('/', function(req,res,next){
-	res.render('index');
+	var findingHotels = Hotel.findAll();
+	var findingRestaurants = Restaurant.findAll();
+	var findingActivities = Activity.findAll();
+	Promise.all([findingActivities,findingRestaurants,findingHotels])
+	.then(function(hotels, restaurants, activities){
+		res.render('index', {hotels: hotels, restaurants: restaurants, activities: activities});
+	})
 })
 
 
